a68d9161e4ee4dd8342ea8f8782e686db1bb8cee
Parvez Kose <parvezkx@amazon.com>
2022-08-04 14:01:48 -0400

    clean up

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,11 @@
+// brandImage: "../src/assets/logo.svg",
+
+import { create } from "@storybook/theming";
+
+export default create({
+  base: SVGFESpotLightElement,
+  brandTitle: "Sort-Tech Design System",
+  brandUrl: "https:www.example.com",
+  brandImage: "",
+  brandTarget: "_self",
+});
